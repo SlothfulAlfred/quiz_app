a30d8ae806854cd92151095dd36ac1650120988d
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-07 15:35:44 -0400

    Set up dependency injection
    Set up the dependency injection using get_it.

--- lib/core/services/local_storage.dart
+++ lib/core/services/local_storage.dart
@@ -1,1 +1,1 @@
-abstract class LocalStorage {}
+abstract class Storage {}
---
+++ lib/locator.dart
@@ -0,0 +1,10 @@
+import 'package:get_it/get_it.dart';
+import 'package:quiz_app/core/services/api.dart';
+
+GetIt locator = GetIt.instance();
+
+bool useFakeData = true;
+
+void setupLocator() {
+  // TODO: register Api as LazySingleton
+}
