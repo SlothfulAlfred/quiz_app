e4ee5d4e7c4918842d55c25429b7b2525e4e12b8
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-08 13:01:38 -0400

    Initialize dependency injection
    [Api] and [AuthenticationService] were registered with get_it so that they can be injected into other classes.

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,10 +1,16 @@
 import 'package:get_it/get_it.dart';
 import 'package:quiz_app/core/services/api.dart';
+import 'package:quiz_app/core/services/authentication.dart';
+import 'package:quiz_app/core/services/fake_api.dart';
+import 'package:quiz_app/core/services/firebase_api.dart';
 
 GetIt locator = GetIt.instance();
 
 bool useFakeData = true;
 
 void setupLocator() {
-  // TODO: register Api as LazySingleton
+  locator.registerLazySingleton<Api>(
+      () => useFakeData ? FakeApi() : FirebaseApi());
+  locator.registerLazySingleton<AuthenticationService>(
+      () => AuthenticationService());
 }
