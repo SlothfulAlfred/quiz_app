54530dd04847018b904834f7ed868325311905a0
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-08 13:01:53 -0400

    Setup dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'quiz_app.dart';
+import 'package:quiz_app/locator.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  setupLocator();
   runApp(QuizApp());
 }
