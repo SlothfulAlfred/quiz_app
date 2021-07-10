5d500d2c77809922b0951e71e77ac190336987b0
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-10 17:48:32 -0400

    Bind HomeView to HomeViewModel
    Previously, HomeView was just a skeleton, now it has been partially implemented for testing.

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,23 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/core/viewModels/home_view_model.dart';
+import 'package:quiz_app/core/viewstate_enum.dart';
+import 'package:quiz_app/locator.dart';
+import 'package:quiz_app/ui/shared/base_view.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({Key? key}) : super(key: key);
+  HomeView({Key? key}) : super(key: key);
+  final HomeViewModel model = locator<HomeViewModel>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        color: Colors.grey[850],
-        child: Center(
-          child: Text(
-            'HomeView',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-            ),
-          ),
-        ),
+      body: BaseView(
+        builder: (BuildContext context, HomeViewModel model, Widget? chlild) {
+          if (model.state == ViewState.idle) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.grey[850],
+                    child: SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: (model.quiz.length > 0)
+                          ? Image.network(
+                              model.quiz[0].imagePath,
+                              fit: BoxFit.cover,
+                            )
+                          : Placeholder(),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: model.fetchImage,
+                    child: Text('Press Me'),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
