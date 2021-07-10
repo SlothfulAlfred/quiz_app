73f0016cc8ee7e7378622a1c1b7de67d4c2bb12b
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-09 23:23:23 -0400

    Update fake_api.dart
    increase the number of cases that are returned by the login method for more thorough testing.

--- lib/core/services/fake_api.dart
+++ lib/core/services/fake_api.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:quiz_app/core/models/api_models.dart';
 import 'package:quiz_app/core/services/api.dart';
+import 'dart:math';
 
 class FakeApi implements Api {
   @override
@@ -24,12 +25,33 @@ class FakeApi implements Api {
   @override
   Future<LoginResponse> login(String username, String password) async {
     await Future.delayed(Duration(seconds: 1));
-    // Always returns a succesful login (for now).
-    if (1 == 1) {
-      return LoginResponse(success: true);
-    } else {
-      return LoginResponse(
-          success: false, error: FirebaseAuthException(code: 'invalid-email'));
+    // Randomly login successfully or throw an error
+    int rand = Random().nextInt(5);
+    switch (rand) {
+      case 0:
+        return LoginResponse(success: true);
+      case 1:
+        return LoginResponse(
+          success: false,
+          error: FirebaseAuthException(code: 'invalid-email'),
+        );
+      case 2:
+        return LoginResponse(
+          success: false,
+          error: FirebaseAuthException(code: 'wrong-password'),
+        );
+      case 3:
+        return LoginResponse(
+          success: false,
+          error: FirebaseAuthException(code: 'user-disabled'),
+        );
+      case 4:
+        return LoginResponse(
+          success: false,
+          error: FirebaseAuthException(code: 'user-not-found'),
+        );
+      default:
+        return LoginResponse(success: true);
     }
   }
 }
