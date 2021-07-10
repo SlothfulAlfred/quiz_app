28963e26a2d17ef38502505ff97d78a4cbfe2914
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-09 23:28:39 -0400

    Add input fields
    Added TextFields for email and password  input and a button for submission. To show these changes, LoginView had to be refactored as a StatefulWidget so that it could rebuild whenever the TextFields change.

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,16 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/core/viewModels/login_view_model.dart';
+import 'package:quiz_app/core/viewstate_enum.dart';
+import 'package:quiz_app/ui/shared/base_view.dart';
+import 'package:quiz_app/ui/widgets/text_field.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  // Is Stateful widget so that controllers can be disposed.
+  final TextEditingController _passwordText = TextEditingController();
+  final TextEditingController _emailText = TextEditingController();
+  final FocusNode _passwordFocus = FocusNode();
+  final FocusNode _emailFocus = FocusNode();
+
+  @override
+  void dispose() {
+    _passwordFocus.dispose();
+    _passwordText.dispose();
+    _emailFocus.dispose();
+    _emailText.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Container(
-          color: Colors.grey[850],
-          child: Text('Login'),
-        ),
+      body: BaseView<LoginViewModel>(
+        builder: _buildContent,
       ),
     );
+  }
+
+  Widget _buildContent(
+      BuildContext context, LoginViewModel model, Widget? child) {
+    if (model.error != null) {
+      _emailText.clear();
+      _passwordText.clear();
+    }
+    if (model.state == ViewState.idle) {
+      return Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 32, 16, 32),
+          child: Column(
+            children: [
+              // email text field
+              InputField(
+                controller: _emailText,
+                focus: _emailFocus,
+              ),
+              // password text field
+              InputField(
+                controller: _passwordText,
+                focus: _passwordFocus,
+                isPassword: true,
+                error: model.error,
+              ),
+              ElevatedButton(
+                onPressed: () =>
+                    model.login(_emailText.text, _passwordText.text),
+                child: Text("Login"),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
   }
 }
