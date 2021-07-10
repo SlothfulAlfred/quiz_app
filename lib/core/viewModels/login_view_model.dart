cd0da6d42f916ba61755f55b9d95b44c9b2d0259
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-09 23:26:29 -0400

    implement error handling in LoginViewModel
    Now handles the errors returned from AuthenticationService.login and sets the proper error emssage.

--- lib/core/viewModels/login_view_model.dart
+++ lib/core/viewModels/login_view_model.dart
@@ -2,11 +2,13 @@ import 'package:quiz_app/core/services/authentication.dart';
 import 'package:quiz_app/core/viewstate_enum.dart';
 import 'package:quiz_app/locator.dart';
 import 'package:quiz_app/ui/routing_constants.dart';
+import 'package:quiz_app/core/services/navigation_service.dart';
 
 import 'base_model.dart';
 
 class LoginViewModel extends BaseModel {
   AuthenticationService _auth = locator<AuthenticationService>();
+  NavigationService _nav = locator<NavigationService>();
 
   /// The error message that should be displayed, if it exists.
   /// i.e. 'email or password is incorrect'
@@ -14,17 +16,38 @@ class LoginViewModel extends BaseModel {
 
   void setErrorMessage(String s) {
     _errorMessage = s;
+    notifyListeners();
   }
 
   String? get error => _errorMessage;
 
-  Future login(String email, String password) async {
+  Future<dynamic> login(String email, String password) async {
     setState(ViewState.busy);
     var result = await _auth.login(email: email, password: password);
-    if (result is bool) {
-      // TODO: Implement Navigation without context
-      // https://www.filledstacks.com/post/navigate-without-build-context-in-flutter-using-a-navigation-service/
-      throw UnimplementedError();
+    if (result == true) {
+      // pushReplacementNamed is used here so that the user cannot
+      // press the back button and return to the login screen.
+      _nav.pushReplacementNamed(homeRoute);
+    } else {
+      // Handling which error message to display
+      switch (result.code) {
+        case 'invalid-email':
+          setErrorMessage('Email or password is incorrect. Please try again.');
+          break;
+        case 'wrong-password':
+          setErrorMessage('Email or password is incorrect. Please try again.');
+          break;
+        case 'user-not-found':
+          setErrorMessage('User cannot be found. Please try again.');
+          break;
+        case 'user-disabled':
+          setErrorMessage('Your account has been disabled.');
+          break;
+        default:
+          setErrorMessage(result.message);
+      }
     }
+    // Now that we are done operations, state is no longer busy
+    setState(ViewState.idle);
   }
 }
