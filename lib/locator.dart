1fd957191d8f6dd35cb4dda38883791e587b9c76
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-09 23:30:18 -0400

    Register new services with get_it

--- lib/locator.dart
+++ lib/locator.dart
@@ -3,8 +3,10 @@ import 'package:quiz_app/core/services/api.dart';
 import 'package:quiz_app/core/services/authentication.dart';
 import 'package:quiz_app/core/services/fake_api.dart';
 import 'package:quiz_app/core/services/firebase_api.dart';
+import 'package:quiz_app/core/services/navigation_service.dart';
+import 'package:quiz_app/core/viewModels/login_view_model.dart';
 
-GetIt locator = GetIt.instance();
+GetIt locator = GetIt.instance;
 
 bool useFakeData = true;
 
@@ -13,4 +15,6 @@ void setupLocator() {
       () => useFakeData ? FakeApi() : FirebaseApi());
   locator.registerLazySingleton<AuthenticationService>(
       () => AuthenticationService());
+  locator.registerLazySingleton<NavigationService>(() => NavigationService());
+  locator.registerLazySingleton<LoginViewModel>(() => LoginViewModel());
 }
