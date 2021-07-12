f4add604d85ee085bfe8bc9a5aa6159b037f38b5
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-12 17:25:37 -0400

    Create shared widgets
    Some utility widgets that wrap display details. These avoid cluttering the Views and are easily reusable.

---
+++ lib/ui/shared/network_image_hero.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+/// Utility class to provide a network image with a hero transition.
+///
+/// [image] must be a url so that it can be passed into [Image.network].
+class NetworkImageHero extends StatelessWidget {
+  /// The path to the image which should be animated.
+  final String image;
+
+  /// A unique identifier for a pair of [Hero] widgets.
+  final String tag;
+
+  /// Optional paramter to specify the final width / height
+  /// of the image.
+  final double? width, height;
+
+  /// Closure that will be called on tap.
+  ///
+  /// Must either push or pop a route otherwise the hero animation
+  /// will not run.
+  final VoidCallback onTap;
+
+  const NetworkImageHero({
+    Key? key,
+    required this.image,
+    required this.tag,
+    required this.onTap,
+    this.width,
+    this.height,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width ?? null,
+      height: height ?? null,
+      child: GestureDetector(
+        onTap: onTap,
+        child: FittedBox(
+          child: Image.network(image),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/shared/quiz_tile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/ui/shared/network_image_hero.dart';
+
+class QuizTile extends StatelessWidget {
+  final String id;
+  final String title;
+  final String? imagePath;
+  final VoidCallback onTap;
+
+  const QuizTile({
+    Key? key,
+    required this.id,
+    required this.onTap,
+    this.title = '',
+    this.imagePath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridTile(
+        child: (imagePath != null)
+            ? NetworkImageHero(image: imagePath!, tag: id, onTap: onTap)
+            : Placeholder(),
+        header: Opacity(opacity: 0.75, child: Text(title)),
+        // TODO: add quiz progress in footer
+      ),
+    );
+  }
+}
