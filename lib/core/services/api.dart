cc545a36243629cae8a3e3502f8a23d444f9bfc2
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-12 17:29:31 -0400

    Changed getQuestions parameter to String
    This change reflects the changes that were made to api_model.dart . It is easier to search through Firestore using the provided autoId strings than custom integer identifiers, especially since order is not very important.

--- lib/core/services/api.dart
+++ lib/core/services/api.dart
@@ -6,8 +6,25 @@ abstract class Api {
 
   Future getQuizzes();
 
-  Future getQuestions(int quizId);
+  Future getQuestions(String quizId);
 
+  // TODO: remove this method due to changes in data structure
+  // This method might have been needed since the plan was to store
+  // a short string as the imagePath for the quizzes, which would act
+  // as an identifier to search Cloud Storage for the image, however
+  // it is better to just store the entire Cloud Storage link in the
+  // database.
+  //
+  // Benefits of this method are:
+  // - Less calls to Cloud Storage
+  // - Faster load time since images are retrieved directly
+  //   (instead of getting the link then retrieving it)
+  //
+  // Detriments of this method are:
+  // - Much harder to change the storage location in the future
+  // - Slightly more data on the database (this isn't a realistic issue for
+  //    an app of this scale, but it might be an issue if it was scaled up to
+  //    contain millions or quizzes. It still should be considered though)
   Future? getPhotoFromPath(String url);
 
   // TODO: consider rmoving this method instead pushing users onto a stream.
