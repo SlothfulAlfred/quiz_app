e486cc082d0ed241ab808be15452e2791a24346b
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-14 22:45:40 -0400

    Create class to standardize spacing
    The VerticalSpace class will help to reduce clutter and keep spacing between widgets standard throughout the whole app.

---
+++ lib/ui/shared/ui_helper.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/// Constant vertical spacing for separating widgets.
+class VerticalSpace {
+  static get large => const SizedBox(
+        height: 50,
+      );
+
+  static get medium => const SizedBox(
+        height: 30,
+      );
+
+  static get small => const SizedBox(
+        height: 18,
+      );
+
+  static get extraSmall => const SizedBox(
+        height: 12,
+      );
+}
