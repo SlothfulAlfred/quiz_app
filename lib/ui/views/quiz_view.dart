c3d1d55ef67036a2970f5e2da111f1c9cb4c765b
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-14 22:41:16 -0400

    Create QuizView
    This view displays a large image of the current quiz, the quiz's description, and a button which will push the questionsRoute.

---
+++ lib/ui/views/quiz_view.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/core/models/api_models.dart';
+import 'package:quiz_app/core/viewModels/quiz_view_model.dart';
+import 'package:quiz_app/ui/shared/base_view.dart';
+import 'package:quiz_app/ui/widgets/network_image_hero.dart';
+import 'package:quiz_app/ui/shared/ui_helper.dart';
+
+class QuizView extends StatelessWidget {
+  final Quiz quiz;
+
+  const QuizView({
+    Key? key,
+    required this.quiz,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double sw = MediaQuery.of(context).size.width;
+    double sh = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(),
+      body: BaseView(
+        builder: (BuildContext context, QuizViewModel model, Widget? _) =>
+            Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Flexible(
+              child: NetworkImageHero(
+                image: quiz.imagePath,
+                tag: quiz.id,
+                onTap: model.onHeroTapped,
+              ),
+            ),
+            VerticalSpace.small,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(24, 0, 24, 32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    quiz.description,
+                    softWrap: true,
+                    // TODO: remember to remove style field when implementing Theme
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  VerticalSpace.medium,
+                  ElevatedButton(
+                    onPressed: () {
+                      model.onStartPressed(quiz);
+                    },
+                    child: Text(
+                      'Start!',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    style: ButtonStyle(
+                      minimumSize: MaterialStateProperty.all(
+                        Size(sw * 0.7, sh * 0.08),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
