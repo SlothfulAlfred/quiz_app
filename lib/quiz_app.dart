13224b4fa0b3c95ccdde862e3f57cb5299e589a5
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-15 21:36:33 -0400

    Register SnackBarService in main app

--- lib/quiz_app.dart
+++ lib/quiz_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/core/services/navigation_service.dart';
+import 'package:quiz_app/core/services/snackbar_service.dart';
 import 'package:quiz_app/ui/router.dart' as router;
 import 'package:quiz_app/ui/routing_constants.dart';
 
@@ -10,6 +11,7 @@ class QuizApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       navigatorKey: locator<NavigationService>().nav,
+      scaffoldMessengerKey: locator<SnackBarService>().key,
       title: 'Quiz App',
       initialRoute: loginRoute,
       onGenerateRoute: router.onGenerateRoute,
