2befabddea1d7c4053729995280c5f75a66d003e
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-15 21:36:58 -0400

    Create initial QuestionsView Stateless Widget

---
+++ lib/ui/views/questions_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/core/models/api_models.dart';
+
+class QuestionsView extends StatelessWidget {
+  const QuestionsView({
+    Key? key,
+    required Quiz quiz,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
