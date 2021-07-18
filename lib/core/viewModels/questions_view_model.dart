30e44a8d04aa2d1e374eb0ac3cfbbb6737e87112
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-17 23:01:28 -0400

    Refactor QuestionsViewModel
    This still does not work.
    
    The first iteration of [QuestionsViewModel] pushed a new route each time a new question was needed, which looked silly because you could see a glimpse of the previous page after each question.
    
    The second iteration has fields [currentQuestion] and [nextQuestion] which were used by [QuestionsView] to build a stack of two pages. The idea was to animate a transition using the stack, however, this did not work because the children rebuilt before the animation had time to complete.
    
    For future development, it would be good to try a nested navigator structure, which would handle the downfalls of the first iteration while being much simpler and more effective than the second iteration.

--- lib/core/viewModels/questions_view_model.dart
+++ lib/core/viewModels/questions_view_model.dart
@@ -17,43 +17,82 @@ class QuestionsViewModel extends BaseModel {
   final NavigationService _nav = locator<NavigationService>();
   final SnackBarService _snackBar = locator<SnackBarService>();
 
-  List<Question>? questions;
-  final String id;
+  late List<Question> questions;
+  final Quiz quiz;
+  final AnimationController controller;
 
-  // flags
   Question? currentQuestion;
+  Question? nextQuestion;
+
   bool finished = false;
 
-  QuestionsViewModel({required this.id}) {
-    _onInit(id);
+  QuestionsViewModel({required this.quiz, required this.controller}) {
+    _onInit(quiz.id);
   }
 
   void _onInit(String id) async {
     setState(ViewState.busy);
     questions = await _api.getQuestions(id);
+
+    // Checks if the quiz is already completed
+    if (questions.length == 0) {
+      _completeQuiz();
+      return;
+    }
+    // runs pushNextQuestion at least twice in order to
+    // make sure currentQuestion is not null.
+    while (currentQuestion == null) {
+      pushNextQuestion();
+    }
     setState(ViewState.idle);
   }
 
   void onChoiceSelected(Choice choice, [SnackBar? snackBar]) {
     if (choice.isCorrect) {
-      // TODO: implement transition to next question
+      pushNextQuestion();
     } else {
       _snackBar.showSnackBar(snackBar!);
     }
   }
 
-  void setQuestion() {
-    setState(ViewState.busy);
+  Future _driveAnimation() async {
     try {
-      int x = Random().nextInt(questions!.length);
-      currentQuestion = questions![x];
-      questions!.removeAt(x);
+      await controller.forward().orCancel;
+    } on TickerCanceled {
+      // View was disposed, catch error.
+      return;
+    }
+  }
+
+  Future pushNextQuestion() async {
+    try {
+      int x = Random().nextInt(questions.length);
+      await _driveAnimation();
+      _setQuestion(questions[x]);
+      questions.removeAt(x);
     } catch (e) {
-      if (questions!.length == 0) {
-        finished = true;
+      // An error has occurred, check if the quiz is completed.
+      if (questions.length == 0 && nextQuestion == null) {
+        _completeQuiz();
+      } else if (questions.length == 0) {
+        // The quiz is not completed, check if it is the last question.
+        await _driveAnimation();
+        _setQuestion(null);
       } else {
+        // An unexpected error has occurred.
         _nav.pushReplacementNamed(errorRoute, arguments: e);
       }
     }
+  }
+
+  void _completeQuiz() {
+    finished = true;
+    notifyListeners();
+  }
+
+  void _setQuestion(Question? newQuestion) {
+    currentQuestion = nextQuestion;
+    nextQuestion = newQuestion;
+    notifyListeners();
   }
 }
