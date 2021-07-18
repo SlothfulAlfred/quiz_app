2321017d70a5b733579a569f270e392e3c28c838
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-17 22:57:21 -0400

    Update routing_constants.dart
    questionsRoute is more consistent with [QuestionsView] and [QuestionsViewModel].

--- lib/ui/routing_constants.dart
+++ lib/ui/routing_constants.dart
@@ -1,5 +1,5 @@
 const String homeRoute = '/';
 const String loginRoute = 'auth';
 const String quizRoute = '/quiz';
-const String questionRoute = '/quiz/questions';
+const String questionsRoute = '/quiz/questions';
 const String errorRoute = '/error';
