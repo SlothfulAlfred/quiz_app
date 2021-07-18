6d7371d5133a0aba7f8dd51f2396e66ca4269ff5
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-17 23:05:26 -0400

    Update registration of routes and dependencies

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,4 +1,8 @@
+// Only being imported so that a variable can be typed
+// [AnimationController].
+import 'package:flutter/animation.dart';
 import 'package:get_it/get_it.dart';
+import 'package:quiz_app/core/models/api_models.dart';
 import 'package:quiz_app/core/services/services.dart';
 import 'package:quiz_app/core/viewModels/view_models.dart';
 
@@ -23,6 +27,7 @@ void setupLocator() {
   locator.registerFactory<QuizViewModel>(() => QuizViewModel());
   // Passes through a parameter to QuestionsViewModel so that it can
   // initialize properly.
-  locator.registerFactoryParam<QuestionsViewModel, String, void>(
-      (String? id, _) => QuestionsViewModel(id: id!));
+  locator.registerFactoryParam<QuestionsViewModel, Quiz, AnimationController>(
+      (Quiz? quiz, AnimationController? controller) =>
+          QuestionsViewModel(quiz: quiz!, controller: controller!));
 }
--- lib/ui/router.dart
+++ lib/ui/router.dart
@@ -3,6 +3,7 @@ import 'package:quiz_app/core/models/api_models.dart';
 import 'package:quiz_app/ui/routing_constants.dart';
 import 'package:quiz_app/ui/views/home_view.dart';
 import 'package:quiz_app/ui/views/login_view.dart';
+import 'package:quiz_app/ui/views/questions_view.dart';
 import 'package:quiz_app/ui/views/quiz_view.dart';
 
 /// Handles all of the route management for the app
@@ -12,11 +13,19 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case loginRoute:
       return MaterialPageRoute(builder: (_) => LoginView());
+
     case homeRoute:
       return MaterialPageRoute(builder: (_) => HomeView());
+
     case quizRoute:
       var quiz = settings.arguments as Quiz;
       return MaterialPageRoute(builder: (_) => QuizView(quiz: quiz));
+
+    case questionsRoute:
+      var args = settings.arguments as Map;
+      var quiz = args['quiz'] as Quiz;
+      return MaterialPageRoute(builder: (_) => QuestionsView(quiz: quiz));
+
     default:
       // TODO: Return UndefinedRoute
       return MaterialPageRoute(builder: (_) => LoginView());
