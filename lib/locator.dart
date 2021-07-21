182e733c8e4884cf272b8dd30a5ed9c848a29546
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-20 21:40:31 -0400

    Remove QuestionsViewModel from dependency registry
    Since [QuestionsViewModel] now has some UI logic, and all of the business logic, it does not need any dependencies to work.

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,8 +1,4 @@
-// Only being imported so that a variable can be typed
-// [AnimationController].
-import 'package:flutter/animation.dart';
 import 'package:get_it/get_it.dart';
-import 'package:quiz_app/core/models/api_models.dart';
 import 'package:quiz_app/core/services/services.dart';
 import 'package:quiz_app/core/viewModels/view_models.dart';
 
@@ -25,9 +21,4 @@ void setupLocator() {
   locator.registerLazySingleton<LoginViewModel>(() => LoginViewModel());
   locator.registerFactory<HomeViewModel>(() => HomeViewModel());
   locator.registerFactory<QuizViewModel>(() => QuizViewModel());
-  // Passes through a parameter to QuestionsViewModel so that it can
-  // initialize properly.
-  locator.registerFactoryParam<QuestionsViewModel, Quiz, AnimationController>(
-      (Quiz? quiz, AnimationController? controller) =>
-          QuestionsViewModel(quiz: quiz!, controller: controller!));
 }
