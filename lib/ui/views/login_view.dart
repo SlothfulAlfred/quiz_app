24e686e988f9d6ca10618e3ebb5b257ef62f18a9
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-28 22:09:56 -0400

    Beautify Login page
    The login page was ugly before, now it has an exciting design based around blue-greens.

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/core/viewModels/login_view_model.dart';
 import 'package:quiz_app/core/viewstate_enum.dart';
 import 'package:quiz_app/ui/shared/base_view.dart';
+import 'package:quiz_app/ui/shared/ui_helper.dart';
 import 'package:quiz_app/ui/widgets/text_field.dart';
 
 class LoginView extends StatefulWidget {
@@ -28,6 +29,7 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(),
       body: BaseView<LoginViewModel, void, void>(
         builder: _buildContent,
@@ -49,23 +51,106 @@ class _LoginViewState extends State<LoginView> {
           padding: const EdgeInsets.fromLTRB(16, 32, 16, 32),
           child: Column(
             children: [
-              // email text field
-              InputField(
-                controller: _emailText,
-                focus: _emailFocus,
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Login',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4!
+                          .copyWith(fontSize: 34),
+                    ),
+                    HorizontalSpace.small,
+                    FlutterLogo(
+                      size: 50,
+                    ),
+                  ],
+                ),
               ),
-              // password text field
-              InputField(
-                controller: _passwordText,
-                focus: _passwordFocus,
-                isPassword: true,
-                error: model.error,
+              VerticalSpace.medium,
+              // Gives the cool elevated effect.
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Email Text Field.
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: InputField(
+                        controller: _emailText,
+                        focus: _emailFocus,
+                      ),
+                    ),
+                    // Divider to separate the two fields
+                    Divider(
+                      height: 2,
+                      color: Colors.blueGrey,
+                    ),
+                    // password text field
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: InputField(
+                        controller: _passwordText,
+                        focus: _passwordFocus,
+                        isPassword: true,
+                      ),
+                    ),
+                  ],
+                ),
+                decoration: BoxDecoration(
+                  boxShadow: [BoxShadow(blurRadius: 0.5)],
+                  color: Colors.grey[100],
+                ),
               ),
-              ElevatedButton(
-                onPressed: () =>
-                    model.login(_emailText.text, _passwordText.text),
-                child: Text("Login"),
+              VerticalSpace.small,
+              // This is the error text, will not render if there is no error.
+              if (model.error != null)
+                Text(
+                  model.error!,
+                  style: Theme.of(context)
+                      .textTheme
+                      .subtitle1!
+                      .copyWith(color: Theme.of(context).errorColor),
+                ),
+              VerticalSpace.small,
+              // Wraps an [ElevatedButton] to provide a gradient effect.
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.tealAccent,
+                      Colors.pinkAccent,
+                    ],
+                  ),
+                ),
+                child: ElevatedButton(
+                  onPressed: () =>
+                      model.login(_emailText.text, _passwordText.text),
+                  child: Text(
+                    "Login",
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    minimumSize: Size.fromHeight(50),
+                  ),
+                ),
               ),
+              VerticalSpace.small,
+              TextButton(
+                child: Text(
+                  'Forgot your password?',
+                  style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                        color: Colors.blueGrey,
+                      ),
+                ),
+                onPressed: () {},
+              )
             ],
           ),
         ),
