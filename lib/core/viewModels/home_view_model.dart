abcea07024600a7a8eecf744ff670ba5e83470fa
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-07-31 22:46:13 -0400

    Update view on exiting QuestionsViewModel
    This lets the progress bar rebuild to adjust for any questions that might have been answered.

--- lib/core/viewModels/home_view_model.dart
+++ lib/core/viewModels/home_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:quiz_app/core/models/api_models.dart';
 import 'package:quiz_app/core/services/navigation_service.dart';
 import 'package:quiz_app/core/services/quiz_service.dart';
+import 'package:quiz_app/core/services/user_service.dart';
 import 'package:quiz_app/core/viewstate_enum.dart';
 import 'package:quiz_app/locator.dart';
 import 'package:quiz_app/core/viewModels/base_model.dart';
@@ -9,6 +10,7 @@ import 'package:quiz_app/ui/routing_constants.dart';
 class HomeViewModel extends BaseModel {
   NavigationService _nav = locator<NavigationService>();
   QuizService _quizzes = locator<QuizService>();
+  UserService _user = locator<UserService>();
 
   HomeViewModel() {
     getQuizzes();
@@ -23,6 +25,19 @@ class HomeViewModel extends BaseModel {
   }
 
   void heroTapped(Quiz quiz) {
-    _nav.pushNamed(quizRoute, arguments: quiz);
+    // Pushes the quiz route and then signals for [HomeView] to
+    // rebuild so that the progress bar will be updated.
+    _nav
+        .pushNamed(quizRoute, arguments: quiz)
+        .then((value) => notifyListeners());
+  }
+
+  int getCompletedForQuiz(String id) {
+    int completed = 0;
+    var quizProgress = _user.getProgressForQuiz(id);
+    for (bool isAnswered in quizProgress.values) {
+      if (isAnswered) completed++;
+    }
+    return completed;
   }
 }
