9dd21d6c8e7ea1e5d3ab8180c431d2cc7b0bd89a
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-08-01 21:07:02 -0400

    Add drawer to HomeView
    The drawer shows all of the user's current progress at a glance. This is not really a necessary feature but I wanted to add it in to get some experience since drawers are very common in mobile apps.

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -3,16 +3,37 @@ import 'package:quiz_app/core/viewModels/home_view_model.dart';
 import 'package:quiz_app/core/viewstate_enum.dart';
 import 'package:quiz_app/locator.dart';
 import 'package:quiz_app/ui/shared/base_view.dart';
+import 'package:quiz_app/ui/shared/ui_helper.dart';
 import 'package:quiz_app/ui/widgets/quiz_tile.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   HomeView({Key? key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
   final HomeViewModel model = locator<HomeViewModel>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Quiz App'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: model.openSettings,
+            tooltip: 'open settings',
+          )
+        ],
+      ),
+      drawer: _buildDrawer(),
+      // When the drawer opens, update it so that it builds with the
+      // newest information.
+      onDrawerChanged: (bool opened) =>
+          (opened == true) ? setState(() {}) : null,
       body: BaseView(
         builder: (BuildContext context, HomeViewModel model, Widget? chlild) {
           if (model.state == ViewState.idle) {
@@ -29,10 +50,10 @@ class HomeView extends StatelessWidget {
                           title: quiz.title,
                           length: quiz.length,
                           completed: model.getCompletedForQuiz(quiz.id),
-                          // Anonymous function that  passes the current quiz
-                          // to heroTapped. This makes the function is a VoidCallback
-                          // and can therefore be used in [GestureDetector] onTap callbacks.
                           onTap: () {
+                            // Pushes a [QuizView] initialized with the given quiz.
+                            // After the page is popped, calls [notifyListeners] to
+                            // rebuild [HomeView].
                             model.heroTapped(quiz);
                           },
                         ))
@@ -46,6 +67,54 @@ class HomeView extends StatelessWidget {
             );
           }
         },
+      ),
+    );
+  }
+
+  Widget _buildDrawer() {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Text(
+              'Your Progress',
+              style: TextStyle(
+                color: Colors.blueGrey,
+                fontWeight: FontWeight.bold,
+                fontSize: 32,
+              ),
+            ),
+          ),
+          for (var quiz in model.quiz)
+            Column(
+              children: [
+                Divider(
+                  color: Colors.black,
+                ),
+                Text(
+                  quiz.title,
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                VerticalSpace.small,
+                Container(
+                  child: ProgressBar(
+                      quiz.length, model.getCompletedForQuiz(quiz.id)),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(),
+                  ),
+                ),
+                VerticalSpace.small,
+              ],
+            ),
+          Divider(
+            color: Colors.black,
+          ),
+        ],
       ),
     );
   }
