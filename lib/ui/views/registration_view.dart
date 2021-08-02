1b6e2176256f768abf4c3c056832addcb8d766f1
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-08-01 21:12:18 -0400

    Create RegistrationView
    Very similar to [LoginView] except with more fields.
    
    'views.dart' was created to reduce clutter in 'router.dart', which has to import all views.

---
+++ lib/ui/views/registration_view.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/core/viewModels/registration_view_model.dart';
+import 'package:quiz_app/ui/shared/base_view.dart';
+import 'package:quiz_app/ui/shared/ui_helper.dart';
+import 'package:quiz_app/ui/widgets/text_field.dart';
+
+class RegistrationView extends StatefulWidget {
+  const RegistrationView({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationViewState createState() => _RegistrationViewState();
+}
+
+class _RegistrationViewState extends State<RegistrationView> {
+  // [TextField] controllers and focuses.
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+  final FocusNode _emailFocus = FocusNode();
+  final FocusNode _usernameFocus = FocusNode();
+  final FocusNode _passwordFocus = FocusNode();
+  final FocusNode _confirmPasswordFocus = FocusNode();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _usernameController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    _emailFocus.dispose();
+    _usernameFocus.dispose();
+    _passwordFocus.dispose();
+    _confirmPasswordFocus.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text('Create an account'),
+      ),
+      body: BaseView<RegistrationViewModel, void, void>(
+        builder: (BuildContext context, RegistrationViewModel model, child) =>
+            Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                'Create an account',
+                style: Theme.of(context).textTheme.headline6!,
+              ),
+              VerticalSpace.medium,
+              // Input Area.
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  boxShadow: [BoxShadow(blurRadius: 0.5)],
+                ),
+                // Input Fields.
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      InputField(
+                        controller: _emailController,
+                        focus: _emailFocus,
+                      ),
+                      Divider(),
+                      InputField(
+                        controller: _usernameController,
+                        focus: _usernameFocus,
+                        hintText: 'Username...',
+                      ),
+                      Divider(),
+                      InputField(
+                        controller: _passwordController,
+                        focus: _passwordFocus,
+                        isPassword: true,
+                      ),
+                      Divider(),
+                      InputField(
+                        controller: _confirmPasswordController,
+                        focus: _confirmPasswordFocus,
+                        isPassword: true,
+                        hintText: 'Confirm password...',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              VerticalSpace.medium,
+              // Fills up the space of the error message so it doesn't
+              // shift the create account button when an error arises.
+              (model.error != null)
+                  ? Text(
+                      model.error!,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 16,
+                      ),
+                    )
+                  : Text('', style: TextStyle(fontSize: 16)),
+              VerticalSpace.small,
+              // Create Account button.
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient:
+                      LinearGradient(colors: [Colors.tealAccent, Colors.pink]),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ElevatedButton(
+                  child: Text('Create Account', style: TextStyle(fontSize: 24)),
+                  // Attempts to create an account, business logic handled in
+                  // ViewModel.
+                  onPressed: () => model.createAccount(
+                    _emailController.text,
+                    _usernameController.text,
+                    _passwordController.text,
+                    _confirmPasswordController.text,
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.transparent),
+                    shadowColor: MaterialStateProperty.all(Colors.transparent),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/views/view.dart
@@ -0,0 +1,6 @@
+export 'error_view.dart';
+export 'home_view.dart';
+export 'login_view.dart';
+export 'questions_view.dart';
+export 'quiz_view.dart';
+export 'registration_view.dart';
