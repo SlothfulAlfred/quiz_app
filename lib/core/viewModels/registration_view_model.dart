e5dac6956c5aa36b675a4067aabf445b72f308bd
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-08-02 21:05:36 -0400

    Add internal register method to RegistrationVM
    This method is almost exactly the same as [LoginViewModel().login] in structure but has a slightly different functionality.

--- lib/core/viewModels/registration_view_model.dart
+++ lib/core/viewModels/registration_view_model.dart
@@ -21,12 +21,53 @@ class RegistrationViewModel extends BaseModel {
 
   String? get error => _errorMessage;
 
-  void createAccount(
-      String email, String username, String password, String confirmed) {
+  Future<void> createAccount({
+    required String email,
+    required String username,
+    required String password,
+    required String confirmed,
+  }) async {
     if (password != confirmed) {
       setErrorMessage("Passwords don't match.");
     } else {
-      setErrorMessage('');
+      await _register(email, password, username);
     }
+  }
+
+  /// Attempts to register an account using an email and a password.
+  Future<dynamic> _register(
+    String email,
+    String password,
+    String username,
+  ) async {
+    setState(ViewState.busy);
+    var result = await _auth.register(email: email, password: password);
+    if (result == true) {
+      // pushReplacementNamed is used here so that the user cannot
+      // press the back button and return to the login screen.
+      _nav.pushReplacementNamed(homeRoute);
+    } else {
+      // Handling which error message to display
+      switch (result.code) {
+        case 'invalid-email':
+          setErrorMessage('This email is invalid. Please try again.');
+          break;
+        case 'weak-password':
+          setErrorMessage('This password is too weak. Please try again.');
+          break;
+        case 'operation-not-allowed':
+          setErrorMessage(
+              'Email registration is not allowed. Please contact us at xxx-xxx-xxxx for more information.');
+          break;
+        case 'email-already-in-use':
+          setErrorMessage(
+              'An account already exists for this email. Please try again.');
+          break;
+        default:
+          setErrorMessage(result.message);
+      }
+    }
+    // Now that we are done operations, state is no longer busy
+    setState(ViewState.idle);
   }
 }
