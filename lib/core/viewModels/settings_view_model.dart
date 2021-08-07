0e64afb0399cc4bd6ebb5c9482b29eac5445967c
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-08-06 21:18:08 -0400

    Add openLogin method to SettingsViewModel
    This method will be triggered by a button which will be visible when the user is signed in anonymously.

--- lib/core/viewModels/settings_view_model.dart
+++ lib/core/viewModels/settings_view_model.dart
@@ -1,13 +1,20 @@
-import 'package:quiz_app/core/services/services.dart';
+import 'package:quiz_app/core/services/user_service.dart';
+import 'package:quiz_app/core/services/navigation_service.dart';
 import 'package:quiz_app/core/viewModels/base_model.dart';
 import 'package:quiz_app/locator.dart';
+import 'package:quiz_app/ui/routing_constants.dart';
 
 class SettingsViewModel extends BaseModel {
   UserService _user = locator<UserService>();
+  NavigationService _nav = locator<NavigationService>();
 
   String get userImagePath => _user.profilePicture ?? '';
   String get userEmail => _user.email ?? '';
   String get username => _user.username ?? '';
 
   bool get isAnonymous => _user.isAnonymous;
+
+  void openLogin() {
+    _nav.pushReplacementNamed(loginRoute);
+  }
 }
