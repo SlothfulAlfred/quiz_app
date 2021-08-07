34a9a22729068fa4fb82ee4e05902135b7e1893d
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-08-06 21:16:31 -0400

    Register LoginViewModel as factory
    In preparation for addition of logout feature, see documentation for why this had to be changed.

--- lib/locator.dart
+++ lib/locator.dart
@@ -7,6 +7,13 @@ GetIt locator = GetIt.instance;
 // Toogle the use of the fake Api
 bool useFakeData = true;
 
+// All ViewModels should be registered as factory so that they can be
+// re-accessed. For example, if you register [LoginViewModel] as a
+// singleton, once you pop the route the object will be garbage collected.
+// If you try to navigate to the login page after [LoginViewModel] has been
+// destroyed, it will throw an error. On the other hand, services can be
+// registered as singletons since they are never garbage collected
+// (since they are never called directly by a View).
 void setupLocator() {
   // Services
   locator.registerLazySingleton<Api>(
@@ -19,7 +26,7 @@ void setupLocator() {
   locator.registerLazySingleton<UserService>(() => UserService());
 
   // ViewModels
-  locator.registerLazySingleton<LoginViewModel>(() => LoginViewModel());
+  locator.registerFactory<LoginViewModel>(() => LoginViewModel());
   locator.registerFactory<HomeViewModel>(() => HomeViewModel());
   locator.registerFactory<QuizViewModel>(() => QuizViewModel());
   locator.registerFactory<RegistrationViewModel>(() => RegistrationViewModel());
