7c3dc091fad696ebc9b32a791621d9488f798ce7
SlothfulAlfred <alfred1.mikhael@gmail.com>
2021-08-09 21:59:32 -0400

    Update user progress after exiting QuestionsView
    Saves user progress to Firestore whenever a user exits [QuestionsView].

--- lib/core/viewModels/quiz_view_model.dart
+++ lib/core/viewModels/quiz_view_model.dart
@@ -1,12 +1,11 @@
 import 'package:quiz_app/core/models/api_models.dart';
 import 'package:quiz_app/core/services/navigation_service.dart';
+import 'package:quiz_app/core/services/user_service.dart';
 import 'package:quiz_app/core/viewModels/base_model.dart';
 import 'package:quiz_app/locator.dart';
 import 'package:quiz_app/ui/routing_constants.dart';
 
 class QuizViewModel extends BaseModel {
-  NavigationService _nav = locator<NavigationService>();
-
   void onHeroTapped() {
     //_nav.pop();
   }
@@ -17,6 +16,16 @@ class QuizViewModel extends BaseModel {
       arguments: {
         'quiz': quiz,
       },
-    );
+      // Saves user progress after exiting [QuestionsView].
+    ).then((_) => _user.updateUserInformation(
+          key: 'progress',
+          value: _user.progress,
+          uid: _user.uid,
+        ));
   }
+
+  // Private Interface
+
+  NavigationService _nav = locator<NavigationService>();
+  UserService _user = locator<UserService>();
 }
